45caca1ae99f896dff34b21ac131b18ae4c2a9b8
qduval <qduval@murex.com>
2019-01-25 16:10:09 +0100

    LRU cache

--- LRUCache.py
+++ LRUCache.py
@@ -16,21 +16,69 @@ def read_resource_cached(resource_name):
     return read_resource(resource_name)
 
 
+class LRUCache:
+    def __init__(self, max_size):
+        self.dict = OrderedDict()
+        self.max_size = max_size
+
+    def __len__(self):
+        return len(self.dict)
+
+    def __contains__(self, key):
+        if key in self.dict:
+            self.dict.move_to_end(key, last=False)
+            return True
+        return False
+
+    def __setitem__(self, key, value):
+        self.dict[key] = value
+        self.dict.move_to_end(key, last=False)
+        if len(self) > self.max_size:
+            self.dict.popitem(last=True)
+
+    def __getitem__(self, key):
+        val = self.dict.get(key)
+        if val is not None:
+            self.dict.move_to_end(key, last=False)
+        return val
+
+
+'''
 class LRUCache(OrderedDict):
     def __init__(self, max_size):
-        super(OrderedDict, self).__init__()
+        super().__init__()
         self.max_size = max_size
+
+    def __contains__(self, key):
+        try:
+            super().move_to_end(key, last=False)
+            return True
+        except KeyError:
+            return False
 
     def __setitem__(self, key, value):
         super().__setitem__(key, value)
+        super().move_to_end(key, last=True)
         if len(self) > self.max_size:
-            self.popitem(last=False)
-        # TODO - return something?
+            super().popitem(last=False)
+
+    def __getitem__(self, key):
+        val = super().get(key)
+        # print(self.dict, key, val)
+        if val is not None:
+            super().move_to_end(key, last=False)
+        return val
+'''
 
 
 def test_lru_cache():
-    resources = ["Fibo.py", "IntegerBreak.py", "NLPFastText.py", "Simulation.py", "Simulation2.py"] * 4
+    resources = ["Fibo.py", "IntegerBreak.py", "NLPFastText.py", "Simulation.py", "Simulation2.py"] * 10
     np.random.shuffle(resources)
+
+    # resources = ['Simulation2.py', 'NLPFastText.py', 'Fibo.py', 'IntegerBreak.py', 'Simulation.py', 'Simulation2.py', 'Fibo.py', 'Fibo.py', 'IntegerBreak.py', 'Fibo.py', 'NLPFastText.py', 'Simulation2.py', 'NLPFastText.py', 'NLPFastText.py', 'Simulation.py', 'IntegerBreak.py', 'Simulation.py', 'Simulation2.py', 'IntegerBreak.py', 'Simulation.py']
+    print(resources)
+
+    print("-" * 50)
 
     for resource in resources:
         read_resource_cached(resource)
@@ -42,7 +90,8 @@ def test_lru_cache():
         if resource not in cache:
             cache[resource] = read_resource(resource)
         else:
-            print("HIT", resource)
+            # print("HIT", resource)
+            pass
 
 
 test_lru_cache()
