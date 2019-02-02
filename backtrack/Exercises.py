a3f874df1e7b2897154bcd61a6631a8eed1691fa
Duval Quentin <senmenty@gmail.com>
2019-02-02 18:38:21 +0100

    coin change with backtracking

--- backtrack/Exercises.py
+++ backtrack/Exercises.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import bisect
 import functools
 import itertools
 from typing import *
@@ -116,11 +117,8 @@ def keypad_words(digits, dictionary) -> List[str]:
 """
 Minimum coin change:
 https://leetcode.com/problems/coin-change/
+The backtracking one beats the hell out of the memoization implementations
 """
-
-
-def min_coin_change(coins, amount):
-    pass
 
 
 def min_coin_change_dp_top_bottom(coins, amount):
@@ -140,6 +138,38 @@ def min_coin_change_dp_bottom_up(coins, amount):
         memo[remaining] = 1 + min(subs, default=float('inf'))
     return memo[-1]
 
+
+def min_coin_change(coins, amount):
+    if amount == 0:
+        return 0
+
+    coins.sort(reverse=True)
+
+    def backtrack(remaining, i, current_best):
+        if i >= len(coins):
+            return float('inf')
+
+        coin = coins[i]
+        if coin > remaining:
+            return backtrack(remaining, i + 1, current_best)
+
+        q, r = divmod(remaining, coin)
+        if r == 0:
+            return q
+
+        if q >= current_best:
+            return float('inf')
+
+        min_res = current_best
+        while q >= 0:
+            sub_res = q + backtrack(r, i + 1, current_best=min_res - q)
+            min_res = min(min_res, sub_res)
+            r += coin
+            q -= 1
+
+        return min_res
+
+    return backtrack(remaining=amount, i=0, current_best=float('inf'))
 
 
 """
