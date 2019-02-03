edb6cecef416178651f34b2d4a9efe20e4d38df9
Duval Quentin <senmenty@gmail.com>
2019-02-03 21:01:09 +0100

    stacks

--- stack/Exercises.py
+++ stack/Exercises.py
@@ -57,3 +57,29 @@ def longest_valid_parentheses(s: str) -> int:
                 opened = 0
         return longest
     return max(find_longest(s, '('), find_longest(s[::-1], ')'))
+
+
+def longest_valid_parentheses(s: str) -> int:
+    """
+    Stack based implementation:
+    - Keep the index of the opening '('
+    - Pop the index when we meet a ')'
+    - If nothing to pop, then retart at next position
+    """
+    longest = 0
+    stack = [-1]  # Last valid point
+    i = 0
+    while i < len(s):
+        c = s[i]
+        if c == '(':
+            stack.append(i)
+            i += 1
+        elif len(stack) == 1:
+            while i < len(s) and s[i] != '(':
+                i += 1
+            stack[-1] = i - 1
+        else:
+            stack.pop()
+            longest = max(longest, i - stack[-1])
+            i += 1
+    return longest
