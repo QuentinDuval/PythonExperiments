f20e9ac4be5c23b7bc08f14322b510f71487e0cd
Duval Quentin <senmenty@gmail.com>
2019-02-03 20:01:59 +0100

    binary search

--- divide/Exercises.py
+++ divide/Exercises.py
@@ -1,4 +1,5 @@
 from collections import *
+from typing import *
 
 
 """
@@ -22,7 +23,43 @@ Find the number replicated several times in a list of number holding otherwise u
 """
 
 
-# TODO
+def find_duplicate(nums: List[int]) -> int:
+    """
+    Proof:
+    Pigeon hole principle states that there must be one hole with 2 pigeons
+
+    Find by Divide and conquer:
+    - Take the average value of min(nums) and max(nums)
+    - Count the number of elements lower than this median
+    - Count the number of elements higher than this median
+    - Recurse in the bigger half
+
+    Complexity is O(n * log n)
+    """
+
+    def count(lo, mid, hi):
+        l_count = r_count = m_count = 0
+        for n in nums:
+            if lo <= n and n <= hi:
+                if n < mid:
+                    l_count += 1
+                elif n > mid:
+                    r_count += 1
+                else:
+                    m_count += 1
+        return l_count, m_count, r_count
+
+    lo = 1
+    hi = len(nums) - 1
+    while lo <= hi:
+        mid = lo + (hi - lo) // 2
+        l_count, m_count, r_count = count(lo, mid, hi)
+        if m_count > 1:
+            return mid
+        elif l_count > mid - lo:
+            hi = mid - 1
+        else:
+            lo = mid + 1
 
 
 """
@@ -31,3 +68,41 @@ Find the longest contiguous sub-sequence that contains at least K of each of the
 
 
 # TODO
+
+
+"""
+Design a calendar: https://leetcode.com/problems/my-calendar-i/
+"""
+
+
+Slot = namedtuple('Slot', ['start', 'end'])
+
+
+class MyCalendar:
+    def __init__(self):
+        self.slots = []
+
+    def book(self, start: int, end: int) -> bool:
+        slot = Slot(start, end)
+        i = self.lower_bound(start)
+        if i < len(self.slots):
+            if self.slots[i].start < end:
+                return False
+
+        if i > 0:
+            if self.slots[i - 1].end > start:
+                return False
+
+        self.slots.insert(i, slot)
+        return True
+
+    def lower_bound(self, start):
+        lo = 0
+        hi = len(self.slots) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if start < self.slots[mid].start:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
