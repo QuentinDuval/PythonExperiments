02da457d12543433df3b07c7cabfb1c50a9bbd6a
Duval Quentin <senmenty@gmail.com>
2019-02-07 00:01:42 +0100

    dynamic programming

---
+++ dp/ShortestSupersequence.py
@@ -0,0 +1,1 @@
+# TODO - shortest string containing both string s1 and s2 as subsequence
---
+++ dp/ShortestSuperstringNPHard.py
@@ -0,0 +1,43 @@
+from functools import lru_cache
+from typing import *
+
+
+def shortestSuperstring(strings: List[str]) -> str:
+    """
+    Dynamic programming
+
+    Reduce the problem of find the best concatenation for [1, 2, 3] to:
+    - Choose 1 and recurse on [2, 3], and put 1 at the start or at the end of this sub-solution
+    - Choose 2 and recurse on [1, 3], and put 2 at the start or at the end of this sub-solution
+    - Etc
+    """
+
+    def concat(s1, s2):
+        l = min(len(s1), len(s2))
+        for i in reversed(range(1, l + 1)):
+            if s1.endswith(s2[:i]):
+                return s1 + s2[i:]
+        return s1 + s2
+
+    @lru_cache(maxsize=None)
+    def visit(visited):
+        best_score = float('inf')
+        best_suffix = ""
+
+        for i, s in enumerate(strings):
+            if not (1 << i) & visited:
+                sub_suffix = visit((1 << i) | visited)
+
+                suffix = concat(s, sub_suffix)
+                if len(suffix) < best_score:
+                    best_score = len(suffix)
+                    best_suffix = suffix
+
+                suffix = concat(sub_suffix, s)
+                if len(suffix) < best_score:
+                    best_score = len(suffix)
+                    best_suffix = suffix
+
+        return best_suffix
+
+    return visit(0)
