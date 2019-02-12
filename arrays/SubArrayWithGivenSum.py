e50fa272b074e86b9b28cc5297c29a4eecf3faa6
Duval Quentin <senmenty@gmail.com>
2019-02-12 23:41:07 +0100

    moar problems, this time from geeksforgeeks

--- arrays/SubArrayWithGivenSum.py
+++ arrays/SubArrayWithGivenSum.py
@@ -1,5 +1,7 @@
 """
 https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0
+
+Given an unsorted array A of size N of non-negative integers, find a continuous sub-array which adds to a given number.
 """
 
 
