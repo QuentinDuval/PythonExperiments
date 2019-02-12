d7477e790280d287b9cb5c945b739bb4e29d7e5c
qduval <qduval@murex.com>
2019-02-12 17:37:01 +0100

    scramble strings

--- backtrack/ScrambleStrings.py
+++ backtrack/ScrambleStrings.py
@@ -18,24 +18,23 @@ class Solution:
         Algorithm 1
         -----------
         Search exhaustively for a scrambling of "s1" that matches "s1".
+        The key problem is that there are two many solutions => backtracking with pruning is needed
 
-        (?) How do you generate all possibilities without duplicates?
-            This repeats the solutions:
+        (?) How do you generate all possibilities without duplicates? This repeats the solutions:
 
-        def visit(s):
-            if len(s) <= 1:
-                return set([s])
+            def visit(s):
+                if len(s) <= 1:
+                    return set([s])
 
-            solutions = set()
-            for i in range(1, len(s)):
-                lefts = visit(s[:i])
-                rights = visit(s[i:])
-                for l in lefts:
-                    for r in rights:
-                        solutions.add(l + r)
-                        solutions.add(r + l)
-            return solutions
-
+                solutions = set()
+                for i in range(1, len(s)):
+                    lefts = visit(s[:i])
+                    rights = visit(s[i:])
+                    for l in lefts:
+                        for r in rights:
+                            solutions.add(l + r)
+                            solutions.add(r + l)
+                return solutions
 
         Algorithm 2
         -----------
@@ -64,5 +63,29 @@ class Solution:
 
         return visit(s1, s2)
 
+    def isScrambleDP(self, s1: str, s2: str) -> bool:
+        """
+        Algorithm 3
+        -----------
+        Dynamic programming
+        """
+
+        n = len(s1)
+        match = [[[False for k in range(n + 1)] for j in range(n)] for i in range(n)]
+        for i in range(n):
+            for j in range(n):
+                match[i][j][1] = s1[i] == s2[j]
+
+        for k in range(2, n + 1):
+            for i in range(n - k + 1):
+                for j in range(n - k + 1):
+                    match[i][j][k] = False
+                    for l in range(1, k):
+                        if match[i][j][l] and match[i + l][j + l][k - l]:
+                            match[i][j][k] = True
+                        elif match[i][j + k - l][l] and match[i + l][j][k - l]:
+                            match[i][j][k] = True
+
+        return match[0][0][n]
 
 
