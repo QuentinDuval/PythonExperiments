ee2b5a516ae6a2bcb779789cbc0317a3dcb12de9
duvalquentin <senmenty@gmail.com>
2019-03-10 09:41:15 +0100

    Word embeddings

--- dl/WordEmbeddings.py
+++ dl/WordEmbeddings.py
@@ -1,6 +1,28 @@
 from annoy import AnnoyIndex
+import fastText
+
+# https://fasttext.cc/docs/en/english-vectors.html
+
+embedding_size = 10
+
+model = fastText.train_unsupervised(input='WordEmbeddingsInput.txt', dim=embedding_size)
+for word in model.get_words():
+    word_id = model.get_word_id(word)
+    embedding = model.get_word_vector(word)
+    print(word, word_id, embedding)
+
 
 # To query for nearest neighbors
-index = AnnoyIndex(3, metric='euclidian')
 
-# Show the surrounding words
+index = AnnoyIndex(embedding_size)
+for word in model.get_words():
+    index.add_item(model.get_word_id(word), model.get_word_vector(word))
+index.build(10)
+# index.save('annoy.ann')
+
+# index.load('annoy.ann')
+while True:
+    a, b, c = input("query>").split(' ')
+    a, b, c = [model.get_word_vector(w) for w in [a, b, c]]
+    result = index.get_nns_by_vector(vector=c + (b - a), n=10)
+    print([model.get_words()[word_i] for word_i in result])
