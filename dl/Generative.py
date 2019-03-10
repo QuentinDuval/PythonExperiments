b43f602716eac0a568a081ad126ff190d4fed6ca
duvalquentin <senmenty@gmail.com>
2019-03-10 10:33:28 +0100

    ideas

--- dl/Generative.py
+++ dl/Generative.py
@@ -1,6 +1,22 @@
+import torch.nn as nn
+
 import nltk
 
 # markov chain
 # RNN
 
-nltk.word_tokenize()
+# nltk.word_tokenize()
+
+
+class Writer(nn.Module):
+    def __init__(self, embedding_size):
+        self.embeddings = nn.Embedding(embedding_dim=embedding_size)
+        # create a RNN that outputs a character
+
+    def forward(self, before):
+        # pass the characters through the embedding
+        # pass through the RNN
+        # output the best character
+        pass
+
+
