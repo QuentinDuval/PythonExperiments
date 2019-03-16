8842e65e85ff0cc3f323808a63480a04ef7b6787
duvalquentin <senmenty@gmail.com>
2019-03-16 11:51:19 +0100

    Sequential model is pretty okay now (once we add drop_out)

--- Learning/Classifier6.py
+++ Learning/Classifier6.py
@@ -34,47 +34,38 @@ class EmbeddingRnnVectorizer:
 
 
 class RnnClassifier(nn.Module):
-    def __init__(self, vocabulary_len, embedding_size, nb_classes):
+    def __init__(self, vocabulary_len, embedding_size, hidden_size, nb_classes, drop_out):
         super().__init__()
         self.embed = nn.Embedding(vocabulary_len, embedding_size)
-        self.hidden_size = 15
+        self.hidden_size = hidden_size
+        self.drop_out_p = drop_out
         self.rnn = nn.GRU(input_size=embedding_size,
                           hidden_size=self.hidden_size,
                           num_layers=1,
                           batch_first=False,
                           dropout=0)
-        # self.drop_out = nn.Dropout(p=0.2)
         self.output_layer = nn.Linear(self.hidden_size, nb_classes)
         # nn.init.xavier_normal_(self.embed.weight)
         # nn.init.xavier_normal_(self.rnn.all_weights)
         # nn.init.xavier_normal_(self.output_layer.weight)
 
-    def forward(self, x, x_length=None):
+    def forward(self, x):
         batch_size, sequence_len = x.shape
-
         x = self.embed(x)       # Shape is batch_size, sequence_len, embedding_size
         x = x.permute(1, 0, 2)  # Shape is sequence_len, batch_size, embedding_size
 
         init_state = torch.zeros(1, batch_size, self.hidden_size)
         outputs, final_state = self.rnn(x, init_state)
+        x = final_state.squeeze(0)  # shape was: 1, batch_size, hidden_size
 
-        # outputs shape: sequence_len, batch_size, hidden_size
-        # final state shape: 1, batch_size, hidden_size
-
-        x = final_state.squeeze(0)
-        # x = outputs[-1]
-        # x = self.select_column(outputs, x_length)
-
-        # x = self.drop_out(x)
+        x = self.drop_out(x)
         x = self.output_layer(x)
         return fn.softmax(x, dim=-1)
 
-    def select_column(self, rnn_out, x_length):
-        x_length = x_length.long().detach().numpy() - 1
-        out = []
-        for batch_index, column_index in enumerate(x_length):
-            out.append(rnn_out[column_index, batch_index])
-        return torch.stack(out)
+    def drop_out(self, x):
+        if self.drop_out_p > 0:
+            return fn.dropout(x, p=self.drop_out_p, training=self.training)
+        return x
 
 
 def test_model_6(split_seed=None):
@@ -84,19 +75,20 @@ def test_model_6(split_seed=None):
     vectorizer = EmbeddingRnnVectorizer.from_corpus(training_corpus, NltkTokenizer(), min_freq=2, max_length=50)
     vocab_len = vectorizer.get_vocabulary_len()
 
-    model = RnnClassifier(vocabulary_len=vocab_len, embedding_size=15, nb_classes=4)
+    model = RnnClassifier(vocabulary_len=vocab_len, embedding_size=20, hidden_size=20, nb_classes=4, drop_out=0.5)
     predictor = Predictor(model=model, vectorizer=vectorizer, with_gradient_clipping=True, split_seed=split_seed)
-    predictor.fit(training_corpus=training_corpus, learning_rate=0.01, weight_decay=0.0001)
+    predictor.fit(training_corpus=training_corpus, learning_rate=1e-2, weight_decay=1e-4)
     predictor.evaluate(test_corpus=test_corpus)
 
 
 """
-learning_rate=0.01, weight_decay=0.0001
+embedding_size=20, hidden_size=20, nb_classes=4, drop_out=0.5
+learning_rate=1e-2, weight_decay=1e-4
 ------------------------------
-Training (max): 4107/4244 (96.77191328934967%)
-Validation (max): 367/472 (77.7542372881356%)
+Training (max): 3579/4244 (84.33081998114986%)
+Validation (max): 369/472 (78.17796610169492%)
 ------------------------------
-Accuracy: 69.21944035346097 %
+Accuracy: 72.45949926362297 %
 """
 
 # test_model_6(split_seed=0)
