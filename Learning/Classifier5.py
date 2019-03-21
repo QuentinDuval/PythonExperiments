653ea60ea804415aa2a16a2e31ef845379b1c50a
qduval <qduval@murex.com>
2019-03-21 09:34:56 +0100

    Great improvement of the convolutional network

--- Learning/Classifier5.py
+++ Learning/Classifier5.py
@@ -3,52 +3,42 @@ from Learning.Predictor import *
 
 
 class ConvolutionalModel(nn.Module):
-    def __init__(self, vocabulary_len, embedding_size, sentence_size, nb_classes):
+    def __init__(self, vocabulary_len, embedding_size, sentence_size, nb_classes, drop_out=0.0):
         super().__init__()
         self.embedding_size = embedding_size
         self.sentence_size = sentence_size
         self.nb_classes = nb_classes
-        self.embed = nn.Embedding(vocabulary_len, self.embedding_size)
+        self.drop_out_p = drop_out
 
+        self.embed = nn.Embedding(vocabulary_len, self.embedding_size)
         self.convnet = nn.Sequential(
-            nn.Conv1d(in_channels=self.embedding_size, out_channels=self.embedding_size * 2, kernel_size=5),
-            nn.ELU(),
-            nn.Conv1d(in_channels=self.embedding_size * 2, out_channels=self.embedding_size, kernel_size=5),
+            nn.Conv1d(in_channels=self.embedding_size, out_channels=self.embedding_size, kernel_size=5, padding=2),
             nn.ELU()
         )
-
-        '''
-        self.conv1 = nn.Conv1d(in_channels=self.embedding_size,
-                               out_channels=self.embedding_size,
-                               kernel_size=5, padding=2, stride=1)
-        self.conv_relu = nn.ELU()
-        self.max_pool1 = nn.MaxPool1d(kernel_size=3, stride=1, padding=1)
-        '''
-
-        # self.drop_out = nn.Dropout(0.5)
-        self.output_layer = nn.Linear(self.embedding_size * (self.sentence_size - 8), self.nb_classes)
+        self.output_layer = nn.Linear(self.embedding_size * self.sentence_size * 2, self.nb_classes)
 
         nn.init.xavier_normal_(self.embed.weight)
         nn.init.xavier_normal_(self.output_layer.weight)
 
     def forward(self, x):
-        batch_size = x.size()[0]
-        x = self.embed(x)           # Return (batch size, sequence size, embedding size)
-        x = x.permute(0, 2, 1)      # Returns (batch size, embedding size, sequence size)
+        batch_size, sequence_len = x.shape
+        x = self.embed(x)                   # Return (batch size, sequence size, embedding size)
 
-        '''
-        x = self.conv1(x)
-        x = self.conv_relu(x)
-        x = self.max_pool1(x)
-        '''
+        conv = x.permute(0, 2, 1)           # Returns (batch size, embedding size, sequence size)
+        conv = self.convnet(conv)           # Returns (batch size, feature size, sequence size)
+        conv = conv.view((batch_size, -1))  # Returns (batch size, feature size * sequence size)
 
-        x = self.convnet(x)
-        # x = self.drop_out(x)
+        x = x.view((batch_size, -1))        # Returns (batch size, embedding size * sequence size)
+        x = torch.cat((x, conv), dim=1)     # Returns (batch size, (embedding size + feature size) * sequence size)
 
-        x = x.view((batch_size, -1))
-
+        x = self.drop_out(x)
         x = self.output_layer(x)
         return fn.softmax(x, dim=-1)
+
+    def drop_out(self, x):
+        if self.drop_out_p > 0:
+            return fn.dropout(x, p=self.drop_out_p, training=self.training)
+        return x
 
 
 def test_model_5(split_seed=None):
@@ -59,17 +49,18 @@ def test_model_5(split_seed=None):
     vocab_len = vectorizer.get_vocabulary_len()
     sentence_len = vectorizer.max_length
 
-    model = ConvolutionalModel(vocabulary_len=vocab_len, sentence_size=sentence_len, embedding_size=10, nb_classes=3)
+    model = ConvolutionalModel(vocabulary_len=vocab_len, sentence_size=sentence_len,
+                               embedding_size=20, nb_classes=3, drop_out=0.5)
     predictor = Predictor(model=model, vectorizer=vectorizer, with_gradient_clipping=True, split_seed=split_seed)
-    predictor.fit(training_corpus=training_corpus, learning_rate=1e-4, weight_decay=1e-4)
+    predictor.fit(training_corpus=training_corpus, learning_rate=1e-3, weight_decay=1e-4)
     predictor.evaluate(test_corpus=test_corpus)
 
 
 """
-Training (max): 3612/4244 (85.10838831291234%)
-Validation (max): 338/472 (71.61016949152543%)
-------------------------------
-Accuracy: 71.5758468335788 %
+Training (max): 3864/4221 (91.54228855721394%)
+Validation (max): 373/469 (79.53091684434968%)
+--------------------------------------------------
+Accuracy: 73.40267459138187 %
 """
 
 # test_model_5(split_seed=0)
