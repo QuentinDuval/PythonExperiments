a2b3a608337ba353d033733f16da5153485a2d2e
duvalquentin <senmenty@gmail.com>
2019-03-23 17:10:06 +0100

    Does not work well... the data is sorted, but these are not the same numbers!

--- dl/Sorting.py
+++ dl/Sorting.py
@@ -13,7 +13,12 @@ from torch.utils.data import *
 class MLPSorting(nn.Module):
     def __init__(self, input_size, hidden_size):
         super().__init__()
-        self.fc = nn.Sequential(nn.Linear(input_size, hidden_size), nn.ReLU(), nn.Linear(hidden_size, input_size))
+        self.fc = nn.Sequential(
+            nn.Linear(input_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, input_size))
 
     def forward(self, x):
         return self.fc(x)
@@ -24,9 +29,9 @@ class SortingPredictor:
         self.model = model
 
     def fit(self, data_set, epoch: int, learning_rate: float, weight_decay: float):
-        loss_fct = nn.L1Loss() # TODO - another one
+        loss_fct = nn.L1Loss()
         optimizer = optim.Adam(params=self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
-        training_loader = DataLoader(data_set, batch_size=100, shuffle=True)
+        training_loader = DataLoader(data_set, batch_size=500, shuffle=True)
 
         for epoch in range(epoch):
             training_loss = 0
@@ -42,14 +47,15 @@ class SortingPredictor:
 
     def predict(self, sequence):
         sequence = torch.FloatTensor(sequence)
-        return self.model(sequence.unsqueeze(0)).squeeze(0)
+        result = self.model(sequence.unsqueeze(0)).squeeze(0)
+        return [x.item() for x in result]
 
 
 def test_sorting(input_size):
     xs = []
     ys = []
-    for _ in range(10000):
-        x = np.random.randint(0, 1000, input_size)
+    for _ in range(50000):
+        x = np.random.randint(low=0, high=100, size=input_size)
         xs.append(x)
         ys.append(np.array(sorted(x), dtype=np.int64))
 
@@ -57,10 +63,15 @@ def test_sorting(input_size):
     ys = torch.FloatTensor(np.stack(ys))
     data_set = TensorDataset(xs, ys)
 
-    model = MLPSorting(input_size=input_size, hidden_size=40)
+    model = MLPSorting(input_size=input_size, hidden_size=50)
     predictor = SortingPredictor(model=model)
-    predictor.fit(data_set, epoch=10, learning_rate=1e-2, weight_decay=0)
-    print(predictor.predict(np.random.randint(0, 1000, input_size)))
+    predictor.fit(data_set, epoch=20, learning_rate=1e-2, weight_decay=0)
+
+    test_x = np.random.randint(low=0, high=100, size=input_size)
+    test_y = predictor.predict(test_x)
+    print(test_x)
+    print(test_y)
 
 
-test_sorting(input_size=15)
+# TODO - does not work well, the numbers are sorted, but not the same as the input!
+# test_sorting(input_size=5)
