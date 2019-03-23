735ba6d9245ef912603608c22d28ef8e38d6d086
duvalquentin <senmenty@gmail.com>
2019-03-23 20:08:03 +0100

    Trying a different paradigm

--- dl/Sorting2.py
+++ dl/Sorting2.py
@@ -10,10 +10,13 @@ import torch.optim as optim
 from torch.utils.data import *
 
 
-# https://datascience.stackexchange.com/questions/29345/how-to-sort-numbers-using-convolutional-neural-network
+class MLPPermutation2(nn.Module):
+    """
+    Very different model: we want to model the permutations (and not the values) by turning the problem in a
+    classification problem (rather than a regression problem).
 
-
-class MLPPermutation1(nn.Module):
+    The problem is that the output size in O(n²)
+    """
     def __init__(self, input_size, hidden_size):
         super().__init__()
         self.fc = nn.Sequential(
@@ -21,41 +24,23 @@ class MLPPermutation1(nn.Module):
             nn.ReLU(),
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, input_size))
+            nn.Linear(hidden_size, input_size * input_size))
 
-    def forward(self, x, **kwargs):
+    def forward(self, x, apply_softmax=True):
+        batch_size, sequence_size = x.shape
         x = self.fc(x)
-        # x = x.round() # Does not work, cannot be derived
+        x = x.view((batch_size, sequence_size, sequence_size))
+        if apply_softmax:
+            x = fn.softmax(x, dim=-1)
         return x
 
 
-class RNNPermutation1(nn.Module):
-    def __init__(self, hidden_size):
-        super().__init__()
-        self.hidden_size = hidden_size
-        self.rnn = nn.GRU(input_size=1, hidden_size=self.hidden_size)
-        self.fc = nn.Linear(self.hidden_size, 1)
-
-    def forward(self, x, **kwargs):
-        batch_size, sequence_len = x.shape
-        x = x.unsqueeze(dim=2)
-        x = x.permute(1, 0, 2)
-        init_state = torch.zeros(1, batch_size, self.hidden_size)
-        x, final_state = self.rnn(x, init_state)
-        x = x.permute(1, 0, 2)  # batch_size, sequence_len, self.hidden_size
-        x = x.contiguous().view((batch_size * sequence_len, self.hidden_size))
-        x = self.fc(x)
-        x = x.view((batch_size, sequence_len))
-        # x = x.round() # Does not work (cannot be derived)
-        return x
-
-
-class RegressionPredictor:
+class ClassificationPredictor:
     def __init__(self, model):
         self.model = model
 
     def fit(self, data_set, epoch: int, learning_rate: float, weight_decay: float):
-        loss_fct = nn.MSELoss()
+        loss_fct = nn.CrossEntropyLoss()
         optimizer = optim.Adam(params=self.model.parameters(), lr=learning_rate, weight_decay=weight_decay)
         training_loader = DataLoader(data_set, batch_size=500, shuffle=True)
 
@@ -64,7 +49,7 @@ class RegressionPredictor:
             self.model.train()
             for x, target in training_loader:
                 self.model.zero_grad()
-                outputs = self.model(x)
+                outputs = self.model(x, apply_softmax=False)
                 loss = loss_fct(outputs, target)
                 loss.backward()
                 optimizer.step()
@@ -74,15 +59,16 @@ class RegressionPredictor:
     def predict(self, sequence):
         sequence = torch.FloatTensor(sequence)
         result = self.model(sequence.unsqueeze(0)).squeeze(0)
-        return [x.round().item() for x in result]
+        _, predicted = torch.max(result.data, dim=-1)
+        return [sequence[x].item() for x in predicted]
 
 
-def fit_regression(xs, ys, model, epoch, learning_rate, weight_decay):
+def fit_classification(xs, ys, model, epoch, learning_rate, weight_decay):
     xs = torch.FloatTensor(np.stack(xs))
-    ys = torch.FloatTensor(np.stack(ys))
+    ys = torch.LongTensor(np.stack(ys))
     data_set = TensorDataset(xs, ys)
 
-    predictor = RegressionPredictor(model=model)
+    predictor = ClassificationPredictor(model=model)
     predictor.fit(data_set, epoch=epoch, learning_rate=learning_rate, weight_decay=weight_decay)
 
     test_x = np.random.randint(low=0, high=100, size=xs.shape[-1])
@@ -97,8 +83,8 @@ def test_copying(input_size, model, epoch):
     for _ in range(10000):
         x = np.random.randint(low=0, high=100, size=input_size)
         xs.append(x)
-        ys.append(x)
-    fit_regression(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
+        ys.append(np.array(range(len(x)), dtype=np.int64))
+    fit_classification(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
 
 
 def test_reversing(input_size, model, epoch):
@@ -107,18 +93,18 @@ def test_reversing(input_size, model, epoch):
     for _ in range(10000):
         x = np.random.randint(low=0, high=100, size=input_size)
         xs.append(x)
-        ys.append(x[::-1])
-    fit_regression(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
+        ys.append(np.array(range(len(x))[::-1], dtype=np.int64))
+    fit_classification(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
 
 
 def reverse_evens(xs):
-    ys = list(xs)
+    ys = list(range(len(xs)))
     i = 0
     j = len(ys) - 1
     while i < j:
-        while i < len(ys) and ys[i] % 2:
+        while i < len(ys) and xs[ys[i]] % 2:
             i += 1
-        while j >= 0 and ys[j] % 2:
+        while j >= 0 and xs[ys[j]] % 2:
             j -= 1
         if i < j:
             ys[i], ys[j] = ys[j], ys[i]
@@ -134,7 +120,7 @@ def test_reversing_evens(input_size, model, epoch):
         x = np.random.randint(low=0, high=100, size=input_size)
         xs.append(x)
         ys.append(reverse_evens(x))
-    fit_regression(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
+    fit_classification(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
 
 
 def test_sorting(input_size, model, epoch):
@@ -143,20 +129,20 @@ def test_sorting(input_size, model, epoch):
     for _ in range(10000):
         x = np.random.randint(low=0, high=100, size=input_size)
         xs.append(x)
-        ys.append(np.array(sorted(x), dtype=np.int64))
-    fit_regression(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
+        y = list(range(len(x)))
+        y.sort(key=lambda i: x[i])
+        ys.append(np.array(y, dtype=np.int64))
+    fit_classification(xs, ys, model, epoch=epoch, learning_rate=1e-2, weight_decay=0)
 
 
-'''
-test_copying(input_size=5, model=MLPPermutation1(input_size=5, hidden_size=10), epoch=50)
-test_copying(input_size=5, model=RNNPermutation1(hidden_size=10), epoch=50)
+# test_copying(input_size=5, model=MLPPermutation2(input_size=5, hidden_size=10), epoch=10)
+# test_copying(input_size=5, model=RNNPermutation1(hidden_size=10), epoch=50)
 
-test_reversing(input_size=5, model=MLPPermutation1(input_size=5, hidden_size=10), epoch=50)
-test_reversing(input_size=5, model=RNNPermutation1(hidden_size=20), epoch=50)
+# test_reversing(input_size=5, model=MLPPermutation2(input_size=5, hidden_size=10), epoch=10)
+# test_reversing(input_size=5, model=RNNPermutation1(hidden_size=20), epoch=50)
 
-test_reversing_evens(input_size=5, model=MLPPermutation1(input_size=5, hidden_size=10), epoch=50)
-test_reversing_evens(input_size=5, model=RNNPermutation1(hidden_size=10), epoch=100)
+# test_reversing_evens(input_size=5, model=MLPPermutation2(input_size=5, hidden_size=50), epoch=10) # TODO - does not learn
+# test_reversing_evens(input_size=5, model=RNNPermutation1(hidden_size=10), epoch=100)
 
-test_sorting(input_size=5, model=MLPPermutation1(input_size=5, hidden_size=50), epoch=50)
-test_sorting(input_size=5, model=RNNPermutation1(hidden_size=25), epoch=50)
-'''
+# test_sorting(input_size=5, model=MLPPermutation2(input_size=5, hidden_size=25), epoch=100) # TODO - does not learn fully
+# test_sorting(input_size=5, model=RNNPermutation1(hidden_size=25), epoch=50)
