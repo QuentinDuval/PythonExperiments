f27bf4a885824403a83af6297d0e812f8ec6acad
duvalquentin <senmenty@gmail.com>
2019-05-04 10:58:43 +0200

    TODOs

--- arrays/TODOs.py
+++ arrays/TODOs.py
@@ -1,4 +1,5 @@
-
+# https://www.geeksforgeeks.org/must-coding-questions-company-wise/
+# https://www.geeksforgeeks.org/must-do-coding-questions-for-companies-like-amazon-microsoft-adobe/
 
 # https://leetcode.com/problems/number-of-subarrays-with-bounded-maximum/
 # https://leetcode.com/problems/friends-of-appropriate-ages/
