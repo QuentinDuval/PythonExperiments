114bfdf3a9a1488ac2e54e30120f1210053b923e
duvalquentin <senmenty@gmail.com>
2019-05-06 09:34:07 +0200

    StrStr

---
+++ thorough/StrStr.py
@@ -0,0 +1,18 @@
+"""
+Your task is to implement the function strstr.
+
+The function takes two strings as arguments (s,x) and locates the occurrence of the string x in the string s.
+The function returns an integer denoting the first occurrence of the string x i, or -1 if not found.
+"""
+
+
+def strstr(s, x):
+    # We want i - j < len(s) => i < len(s) - j => i < len(s) - len(x) + 1
+    for i in range(0, len(s) - len(x) + 1):
+        match = True
+        for j in range(0, len(x)):
+            if s[i+j] != x[j]:
+                match = False
+        if match:
+            return i
+    return -1
