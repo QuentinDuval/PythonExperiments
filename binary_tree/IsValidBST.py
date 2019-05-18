c54bec4dfdb8fd051016e702a891bbea98eb459e
duvalquentin <senmenty@gmail.com>
2019-05-18 19:42:22 +0200

    Is BST

--- binary_tree/IsValidBST.py
+++ binary_tree/IsValidBST.py
@@ -1,1 +1,27 @@
-# TODO
+"""
+https://practice.geeksforgeeks.org/problems/check-for-bst/1
+"""
+
+
+"""
+#include <functional>
+using Pred = std::function<bool(int)>;
+
+bool isValid(Node* root, Pred lo, Pred hi)
+{
+    if (!root) return true;
+    
+    int val = root->data;
+    return lo(val)
+        && hi(val)
+        && isValid(root->left, lo, [=](int i) { return i <= val; })
+        && isValid(root->right, [=](int i) { return i >= val; }, hi);
+}
+
+bool isBST(Node* root)
+{
+    return isValid(root,
+        [](int i) { return true; },
+        [](int i) { return true; });
+}
+"""
