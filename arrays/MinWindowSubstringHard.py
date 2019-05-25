76793b53cbedea22c53d315f779d5ae8dd54e479
duvalquentin <senmenty@gmail.com>
2019-05-25 10:11:20 +0200

    TODOs

--- arrays/MinWindowSubstringHard.py
+++ arrays/MinWindowSubstringHard.py
@@ -57,13 +57,15 @@ class Solution:
             if not min_window or min_window[1] - min_window[0] > end - start + 1:
                 min_window = start - 1, end
 
+            # Note that at this point, we move start past the preceding point
+
         return s[min_window[0]:min_window[1] + 1] if min_window else ""
 
     def minWindow_2(self, s: str, t: str) -> str:
         """
         Instead of looking for the full pattern when incrementing 'start', you can just look at the letter
         in the pattern and the window and stop when their count is equal
-        => Complexity falls to O(N) and takes 500 ms
+        => Complexity falls to O(N) and takes 160 ms (massive optimization)
         """
 
         if not t:
@@ -72,29 +74,34 @@ class Solution:
         pattern = Counter(t)
         window = Counter()
 
-        def contains(window, pattern):
-            for c, count in pattern.items():
-                if window[c] < count:
-                    return False
-            return True
-
+        match_count = 0
         min_window = None
 
         start = 0
         for end in range(len(s)):
-            window[s[end]] += 1
-            if not contains(window, pattern):
-                continue
-
-            # Key optimisation here:
+            # Key optimization here:
             # You do not need to check the whole pattern, instead just
-            # look for a character whose count is == pattern count
-            while window[s[start]] > pattern[s[start]]:
-                window[s[start]] -= 1
-                start += 1
+            # look for the current character and increase the count of
+            # matched characters
+            if window[s[end]] < pattern[s[end]]:
+                match_count += 1
+            window[s[end]] += 1
 
-            if not min_window or min_window[1] - min_window[0] > end - start:
-                min_window = start, end
+            if match_count == len(t):
+                # Key optimisation here:
+                # You do not need to check the whole pattern, instead just
+                # look for a character whose count is == pattern count
+                while window[s[start]] > pattern[s[start]]:
+                    window[s[start]] -= 1
+                    start += 1
+
+                if not min_window or min_window[1] - min_window[0] > end - start:
+                    min_window = start, end
+
+                # Move past the current window (look for another one)
+                window[s[start]] -= 1
+                match_count -= 1
+                start += 1
 
         return s[min_window[0]:min_window[1] + 1] if min_window else ""
 
