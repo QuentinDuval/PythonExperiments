79a5e3c4a574c50e9f1c77ee8772d7809312ff6d
duvalquentin <senmenty@gmail.com>
2019-05-25 10:33:49 +0200

    OPtims

--- arrays/MinWindowSubstringHard.py
+++ arrays/MinWindowSubstringHard.py
@@ -145,3 +145,45 @@ class Solution:
 
         return s[min_window[0]:min_window[1] + 1] if min_window else ""
 
+    def minWindow_3(self, s: str, t: str) -> str:
+        """
+        Just use plain maps and not Counter
+        => Complexity is O(N) and takes 100 ms (nice optimization)
+        """
+        if not t:
+            return ""
+
+        pattern = {}
+        for c in t:
+            pattern[c] = pattern.get(c, 0) + 1
+        match_count = 0
+        min_window = None
+
+        start = 0
+        for end in range(len(s)):
+            # Key optimization here:
+            # You do not need to check the whole pattern, instead just
+            # look for the current character and increase the count of
+            # matched characters
+            if pattern.get(s[end], 0) > 0:
+                match_count += 1
+            pattern[s[end]] = pattern.get(s[end], 0) - 1
+
+            if match_count == len(t):
+                # Key optimisation here:
+                # You do not need to check the whole pattern, instead just
+                # look for a character whose count is == pattern count
+                while pattern[s[start]] < 0:
+                    pattern[s[start]] += 1
+                    start += 1
+
+                if not min_window or min_window[1] - min_window[0] > end - start:
+                    min_window = start, end
+
+                # Move past the current window (look for another one)
+                pattern[s[start]] += 1
+                match_count -= 1
+                start += 1
+
+        return s[min_window[0]:min_window[1] + 1] if min_window else ""
+
