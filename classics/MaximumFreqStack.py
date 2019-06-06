b34910294bd325a162976f3d3c7f44f488017081
qduval <qduval@murex.com>
2019-06-06 15:24:15 +0200

    Maximum Frequency Stack

---
+++ classics/MaximumFreqStack.py
@@ -0,0 +1,74 @@
+"""
+https://leetcode.com/problems/maximum-frequency-stack
+
+Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+FreqStack has two functions:
+* push(int x), which pushes an integer x onto the stack.
+* pop(), which removes and returns the most frequent element in the stack.
+
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+"""
+
+
+class FreqNode:
+    def __init__(self, value, time):
+        self.value = value
+        self.stack = [time]
+
+    def __lt__(self, other):
+        if len(self.stack) != len(other.stack):
+            return len(self.stack) < len(other.stack)
+        else:
+            return self.stack[-1] < other.stack[-1]
+
+    def __eq__(self, other):
+        return len(self.stack) == other.stack and self.stack[-1] == other.stack[-1]
+
+
+class FreqStack:
+    """
+    The principle look like a Heap but:
+    - we bump the priority by 1 when we add an element that is already there
+    - in case of tie, we want to return the element least arrived
+
+    Clearly we need to retrieve an element by its frequency quickly: need some kind of Heap
+    Clearly we need to find an element by its value quickly: need for some kind of Hash Map
+    Clearly we need to find which element is last arrived: need for some arrival time (but keep the past arrival times as well)
+    """
+
+    def __init__(self):
+        self.next_index = 0  # the arrival time
+        self.values = {}  # value => position in the heap
+        self.heap = [None]  # the heap itself
+
+    def push(self, val: int) -> None:
+        self.next_index += 1
+        node_pos = self.values.get(val, None)
+        if node_pos is None:
+            node = FreqNode(value=val, time=self.next_index)
+            self.heap.append(node)
+            self.swim_up(len(self.heap) - 1)
+        else:
+            node = self.heap[node_pos]
+            node.stack.append(self.next_index)
+            self.swim_up(node_pos)
+
+    def pop(self) -> int:
+        top_node = self.heap[1]
+        top_node.stack.pop()
+        if top_node.stack:
+            self.sink_down(1)
+        else:
+            self.swap(1, len(self.heap) - 1)
+            self.heap.pop()
+            self.sink_down(1)
+        return top_node.value
+
+    def swim_up(self, pos):
+        pass
+
+    def sink_down(self, pos):
+        pass
+
+    def swap(self, pos1, pos2):
+        pass
