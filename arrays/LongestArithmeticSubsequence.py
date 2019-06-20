526b700746ad7df7f36f0ab39c6939489c26e494
qduval <qduval@murex.com>
2019-06-20 14:35:05 +0200

    Longest Arithmetic Subsequence

--- arrays/LongestArithmeticSubsequence.py
+++ arrays/LongestArithmeticSubsequence.py
@@ -1,3 +1,12 @@
+"""
+https://leetcode.com/problems/longest-arithmetic-sequence/
+
+Given an array A of integers, return the length of the longest arithmetic subsequence in A.
+
+Recall that a subsequence of A is a list A[i_1], A[i_2], ..., A[i_k] with 0 <= i_1 < i_2 < ... < i_k <= A.length - 1,
+and that a sequence B is arithmetic if B[i+1] - B[i] are all the same value (for 0 <= i < B.length - 1).
+"""
+
 import bisect
 from collections import defaultdict
 from typing import List
