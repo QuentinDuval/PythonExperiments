b936b3d290b53e2cd66c17254d1cb758cc11caeb
qduval <qduval@murex.com>
2019-07-04 10:35:51 +0200

    Fixed allocation of IDs

--- zookeeper_trial/Locking.py
+++ zookeeper_trial/Locking.py
@@ -3,12 +3,31 @@ import os
 import time
 
 
-class TestClient:
-    # TODO - lack the support for Zookeeper falling
+class IdentifierStream:
+    def __init__(self, zk, reservation_size):
+        self.zk = zk
+        self.reservation_size = reservation_size
+        self.current_id = None
+        self.last_id = None
+        self.counter = zk.Counter("/id", default=1)
 
-    def __init__(self, reservation_size):
+    def __next__(self):
+        # TODO - race condition here if we use counter.value? how does it work in behind?
+        if self.current_id is None or self.current_id >= self.last_id:
+            self.counter += self.reservation_size
+            self.current_id = self.counter.pre_value + 1
+            self.last_id = self.counter.post_value
+        else:
+            self.current_id += 1
+        return self.current_id
+
+
+class TestClient:
+    def __init__(self, zk, reservation_size):
+        self.zk = zk
         self.acquired = {}
         self.keep_looping = True
+        self.id_stream = IdentifierStream(zk, reservation_size)
         self.reservation_size = reservation_size
 
     def loop(self):
@@ -38,25 +57,23 @@ class TestClient:
             time.sleep(10)
             print("Dropping the lead now...")
 
-        children = zk.get_children("/leader")
+        children = self.zk.get_children("/leader")
         if not children:
-            election = zk.Election("/leader", str(os.getpid()))
+            election = self.zk.Election("/leader", str(os.getpid()))
             election.run(when_elected)
         else:
             print("Leader already exists", children[0])
 
     def reserve_id(self):
-        counter = zk.Counter("/id", default=1)
-        counter += 1
-        print(counter.value)
+        print(next(self.id_stream))
 
     def list_acquired_locks(self):
         # ZK will keep listing as children released locks (need this filter)
-        locked_children = [child for child in zk.get_children(path="/object") if self.is_locked(child)]
+        locked_children = [child for child in self.zk.get_children(path="/object") if self.is_locked(child)]
         print(locked_children)
 
     def is_locked(self, lock_id):
-        _, stats = zk.get(path="/object/{child}".format(child=lock_id))
+        _, stats = self.zk.get(path="/object/{child}".format(child=lock_id))
         return stats.numChildren != 0
 
     def lock_object(self, lock_id):
@@ -72,7 +89,7 @@ class TestClient:
             self.acquire(object_id)
 
     def acquire(self, object_id):
-        lock = zk.Lock("/object/{object_id}".format(object_id=object_id), str(os.getpid()))
+        lock = self.zk.Lock("/object/{object_id}".format(object_id=object_id), str(os.getpid()))
         if lock.acquire(blocking=False, ephemeral=True):
             self.acquired[object_id] = lock
             print("Object {object_id} locked".format(object_id=object_id))
@@ -88,17 +105,22 @@ class TestClient:
             self.release(object_id, lock)
 
     def on_zookeeper_status_update(self, state):
+        # TODO - lack the support for Zookeeper falling ?
         # print(state)
         # self.keep_looping = False
         pass
 
 
-if __name__ == '__main__':
+def main():
     zk = KazooClient(hosts='127.0.0.1:2181')
-    client = TestClient(reservation_size=1)
+    client = TestClient(zk, reservation_size=5)
     try:
         zk.add_listener(lambda zk_state: client.on_zookeeper_status_update(zk_state))
         zk.start()
         client.loop()
     finally:
         zk.stop()
+
+
+if __name__ == '__main__':
+    main()
