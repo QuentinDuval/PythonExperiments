d63968f78310a5c97af1ca79e8203903ec10a269
duvalquentin <senmenty@gmail.com>
2019-07-22 21:21:43 +0200

    NP HARD

---
+++ np_hard/GraphColoring.py
@@ -0,0 +1,2 @@
+# TODO - a greedy approach is a good approximation
+#  https://www.geeksforgeeks.org/graph-coloring-set-2-greedy-algorithm/
---
+++ np_hard/Knapsack.py
@@ -0,0 +1,2 @@
+# TODO - For integer values, a pseudo-polynomial approach based on dynamic programming works good
+# TODO - Also list some problems that reduce to Knapsack
