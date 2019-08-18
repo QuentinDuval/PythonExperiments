6aa76ceca7fe1d926366cd8350cbd7b1ece933b9
duvalquentin <senmenty@gmail.com>
2019-08-18 18:52:34 +0200

    Heaps

---
+++ heaps/KthSmallestElementInSortedMatrix.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+
+Given a n x n matrix where each of the rows and columns are sorted in ascending order,
+find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+"""
+
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        # TODO - just use heap
+        pass
