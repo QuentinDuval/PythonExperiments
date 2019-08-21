3e420f6646dd88a62a11e6be31d080aec1abe065
duvalquentin <senmenty@gmail.com>
2019-08-21 16:47:20 +0200

    Count of range sum (BIT) - hard

--- arrays/CountOfRange_HARD.py
+++ arrays/CountOfRange_HARD.py
@@ -5,19 +5,14 @@ Given an integer array nums, return the number of range sums that lie in [lower,
 Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j (i ≤ j), inclusive.
 """
 
-
 from typing import List
 
 
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
         """
-        Thoughts:
-        - the negative numbers means we cannot use a two finger approach (slide a window)
-        - cumulative sum in O(n) to easily compute the partial sums in O(1) => allows O(N**2) algo
-        - we do not have to return the ranges, only their numbers (otherwise would be stock to N**2)
-
-        When stuck, turn to a data structure?
+        Computing the prefix sum in O(n) makes it easy to compute the partial sums in O(1)
+        => allows O(N**2) algo that try all i < j
         """
 
         '''
@@ -39,49 +34,54 @@ class Solution:
         that would, if we substract them to the current cumulative sum, fall in the range
 
         We can accelerate this search using a Hash Map:
-        - hash map from cum_sum to count
-        - try for all values in the range
-        => O(n * (upper - lower + 1)) algorithm
+        But requires to test all numbers in [curr_prefix_sum - upper, curr_prefix_sum - lower]
 
-        We could accelerate this search further using a Tree Map:
-        - tree map from cum_sum to count
-        - range search lower_bound(lower), upper_bound(lower)
-        => O(n * (upper - lower + 1)) algorithm worst case - often faster
+        We could use a Tree Map, but we still have to scan the numbers between
+        [curr_prefix_sum - upper, curr_prefix_sum - lower] (less to test, but still)
+
+        The solution is to use a Data Structure that allows counting in a range:
+        - Binary Index Tree (with mapping of values to reduce memory footprint)
+        - Segment Tree
+
+        Complexity is O(N log N)
+        Beats 45% (248 ms)
         """
 
-        '''
-        cum_sums = [0]
-        for n in nums:
-            cum_sums.append(cum_sums[-1] + n)
+        prefix_sums = [0]
+        for num in nums:
+            prefix_sums.append(prefix_sums[-1] + num)
 
-        count = 0
-        prev_cum_sums = {}
-        for cum_sum in cum_sums:
-            # We want:
-            #       lower <= cum_sum - prev_sum <= upper
-            # =>    cum_sum - upper <= prev_sum <= cum_sum - lower
-            for prev_sum in range(cum_sum - upper, cum_sum - lower + 1):
-                count += prev_cum_sums.get(prev_sum, 0)
-            prev_cum_sums[cum_sum] = prev_cum_sums.get(cum_sum, 0) + 1
-        return count
-        '''
+        # Mapping of the different prefix_sums to indexes on the BIT
+        mapping = {}
+        for i, n in enumerate(sorted(set(s + d for s in prefix_sums for d in [-upper, 0, -lower]))):
+            mapping[n] = i + 1
 
-        """
-        We can make it faster by computing the cumulative sums on the run
-        """
+        # BIT data structure
+        bit_size = len(mapping) + 2
+        bit = [0] * bit_size
 
-        count = 0
-        cum_sum = 0
-        prev_cum_sums = {0: 1}
-        for n in nums:
-            cum_sum += n
-            # We want:
-            #       lower <= cum_sum - prev_sum <= upper
-            # =>    cum_sum - upper <= prev_sum <= cum_sum - lower
-            for prev_sum in range(cum_sum - upper, cum_sum - lower + 1):
-                count += prev_cum_sums.get(prev_sum, 0)
-            prev_cum_sums[cum_sum] = prev_cum_sums.get(cum_sum, 0) + 1
-        return count
+        def add_to_bit(val):
+            val = mapping[val]
+            while val < len(bit):  # Add to all ranges above by adding last bit
+                bit[val] += 1
+                val += val & -val
+
+        def count_up_to(val, inclusive: bool):
+            val = mapping[val] if inclusive else mapping[val] - 1
+            count = 0
+            while val > 0:  # Count all ranges below by removing last bit
+                count += bit[val]
+                val -= val & -val
+            return count
+
+        def range_count(start, end):
+            return count_up_to(end, True) - count_up_to(start, False)
+
+        total_count = 0
+        for prefix_sum in prefix_sums:
+            total_count += range_count(prefix_sum - upper, prefix_sum - lower)
+            add_to_bit(prefix_sum)
+        return total_count
 
         """
         We can make it faster by using a Tree Map as described above
@@ -102,10 +102,4 @@ class Solution:
         }
         return count;
         '''
-
-        """
-        Segment tree
-        """
-
-        # TODO
 
