14d855a524d21fd5c8f628e6dfc9619b7c2de717
qduval <qduval@murex.com>
2019-09-04 11:37:09 +0200

    Minor refactoring

--- crawler/wordpress.py
+++ crawler/wordpress.py
@@ -2,30 +2,27 @@ import os
 import re
 
 import asks
+from asks.sessions import Session
 import trio
 from lxml import etree
 from lxml import html
 
 
-class LinkExtractor:
+class UrlExtractor:
     def __init__(self, domain: str):
         self.domain = domain
         self.parser = etree.HTMLParser()
 
-    async def extract_links(self, url: str, visit_url, queue_url):
+    async def extract_links(self, session: Session, url: str, on_visit_url, add_url_to_queue):
         try:
-            print("Try:", url)
-            response = await asks.get(url)
-            print("Got:", url)
-
+            response = await session.get(url)
             content = response.content.decode('utf-8')
             tree = html.fromstring(content)
-            # tree = etree.parse(io.StringIO(content), self.parser) # Would parse in XML
-            visit_url(url, tree)
+            on_visit_url(url, tree)
             for href in tree.xpath('//a/@href'):
                 url = self.clean_href(href)
                 if url is not None:
-                    queue_url(url)
+                    add_url_to_queue(url)
         except Exception as e:
             print("Failed to parse content of", url, "due to", e)
 
@@ -38,28 +35,29 @@ class LinkExtractor:
 
 
 class Crawler:
-    def __init__(self, domain: str, blog_post_regex: str):
+    def __init__(self, domain: str, blog_post_regex: str, max_connection: int):
         self.domain = domain
         self.queue = []
         self.discovered = set()
         self.visited = {}
         self.blog_post_regex = re.compile(blog_post_regex)
+        self.session = Session(self.domain, connections=max_connection)
+        self.extractor = UrlExtractor(self.domain)
 
     async def collect_links(self, start_url: str):
-        self.add_url(start_url)
-        extractor = LinkExtractor(self.domain)
+        self.add_url_link(start_url)
         while self.queue:
             # TODO - this is a BFS by design... try a DFS by using channels
             async with trio.open_nursery() as nursery:
                 while self.queue:
                     url = self.queue.pop()
-                    nursery.start_soon(extractor.extract_links, url, self.add_blog, self.add_url)
+                    nursery.start_soon(self.extractor.extract_links, self.session, url, self.on_visited, self.add_url_link)
 
-    def add_blog(self, url, content):
+    def on_visited(self, url, content):
         if self.blog_post_regex.match(url):
             self.visited[url] = content
 
-    def add_url(self, url):
+    def add_url_link(self, url):
         if url not in self.discovered:
             self.discovered.add(url)
             self.queue.append(url)
@@ -67,7 +65,7 @@ class Crawler:
 
 class BlogPostExtractor:
     def __init__(self, domain: str, blog_post_regex: str):
-        self.crawler = Crawler(domain=domain, blog_post_regex=blog_post_regex)
+        self.crawler = Crawler(domain=domain, blog_post_regex=blog_post_regex, max_connection=10)
 
     def extract(self, first_url: str, folder: str, file_prefix: str):
         trio.run(self.crawler.collect_links, first_url)
