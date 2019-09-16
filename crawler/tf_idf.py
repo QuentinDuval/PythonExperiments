554a2b86843141fdddd260ecb84caf1f3ea445fe
qduval <qduval@murex.com>
2019-09-16 17:04:02 +0200

    TF-IDF ideas

--- crawler/tf_idf.py
+++ crawler/tf_idf.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from crawler.parsing import NltkTokenizer, list_files_in_folder
 
 from collections import defaultdict
@@ -7,6 +9,12 @@ import os
 
 
 class PostingList:
+    """
+    Posting list for a given word:
+    - Keep a document to number of occurrence number dictionary
+    - Keep the total amount of time a word has been seen
+    """
+
     def __init__(self):
         self.total = 0
         self.postings = defaultdict(int)
@@ -20,8 +28,12 @@ class PostingList:
 
 
 class InvertedIndex:
+    """
+    Maintains an inverted index of documents (blog posts here) from words to documents that contain the words.
+    """
+
     def __init__(self):
-        self.index = defaultdict(PostingList)
+        self.posting_lists = defaultdict(PostingList)
         self.tokenizer = NltkTokenizer()
 
     def add_document(self, file_path: str):
@@ -29,14 +41,33 @@ class InvertedIndex:
         with open(file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 for word in self.tokenizer.tokenize(line):
-                    self.index[word].add_document(file_name)
+                    self.posting_lists[word].add_document(file_name)
+
+    def get_topic(self, document_title: str) -> List[str]:
+        """
+        Return the titles of the document that are the most related to the document_title
+        """
+        pass  # TODO
+
+    def get_documents(self, words: List[str]) -> List[str]:
+        """
+        Return the list of title of the document that are the most related to the words given as input
+        """
+        pass  # TODO
+
+    def get_common_expressions(self) -> List[str]:
+        """
+        Use TF-IDF / correlation measure to return the terms that are the most associated together
+        """
+        pass  # TODO
 
 
 inv_index = InvertedIndex()
 for file_path in list_files_in_folder("posts/fluentcpp"):
     inv_index.add_document(file_path)
 
-for word, posting in sorted(inv_index.index.items(), key=lambda p: p[1].total, reverse=True):
-    print(word)
-    print(posting)
+for i, (word, posting) in enumerate(sorted(inv_index.posting_lists.items(), key=lambda p: p[1].total, reverse=True)):
+    if i < 10:
+        print(word)
+        print(posting)
 
