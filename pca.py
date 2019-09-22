c505f2bf9eeaf1afaa475c223acbee0c3b2e3c72
duvalquentin <senmenty@gmail.com>
2019-09-22 18:03:22 +0200

    PCA using Stochastic Gradient Descent

---
+++ pca.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import *
+
+import numpy as np
+
+
+class PrincipalComponentExtraction(nn.Module):
+    """
+    Equivalent of PCA using an auto-encoder:
+    - Compared to classic PCA, based on SVD, you can less simply ask for K dimension (with a diff to cut to find K)
+    - Here we provide the hidden size we want (no way to find the optimal one)
+    - TODO: But using regularization, you can select only the most important axis (kind of find the K for us as well?)
+    - TODO: this learns the basic probability distribution of a manifold, allowing to do (for instance) de-noising
+            by projecting on that space
+    """
+
+    def __init__(self, feature_size: int, output_size: int):
+        super().__init__()
+        self.encoder = nn.Linear(feature_size, output_size)
+        self.decoder = nn.Linear(output_size, feature_size)
+
+    def forward(self, xs):
+        ys = self.encoder(xs)
+        return self.decoder(ys)
+
+    def fit(self, inputs, nb_epoch: int = 10):
+        self.train()
+        optimizer = optim.Adam(self.parameters(), lr=1e-3, weight_decay=1e-3)
+        criterion = nn.MSELoss()
+        training_set = torch.stack([torch.from_numpy(x) for x in inputs])
+        training_loader = DataLoader(training_set, batch_size=100, shuffle=True)
+        for _ in range(nb_epoch):
+            total_loss = 0.
+            for xs in training_loader:
+                optimizer.zero_grad()
+                ys = self(xs)
+                loss = criterion(ys, xs)           # Compute the error
+                loss.backward()                    # Propagate the gradient
+                optimizer.step()
+                total_loss += loss.item()
+            print(total_loss)
+
+    def transform(self, xs):
+        return self.encode(xs)
+
+    def encode(self, xs):
+        self.eval()
+        with torch.no_grad():
+            xs = torch.stack([torch.from_numpy(x) for x in xs])
+            return self.encoder(xs).numpy()
+
+    def decode(self, xs):
+        self.eval()
+        with torch.no_grad():
+            xs = torch.stack([torch.from_numpy(x) for x in xs])
+            return self.decoder(xs).numpy()
+
+
+def test():
+    def new_point(x, y):
+        return np.array([x, y, 2 * x + y], dtype=np.float32)
+
+    def generate_inputs(size=100):
+        xs = np.random.normal(loc=1, scale=2, size=size)
+        ys = np.random.normal(loc=1, scale=2, size=size)
+        return [new_point(x, y) for x, y in zip(xs, ys)]
+
+    inputs = generate_inputs(size=5000)
+    tests = [new_point(1, 1)]
+
+    pca = PrincipalComponentExtraction(feature_size=3, output_size=2)
+    pca.fit(inputs=inputs, nb_epoch=200)
+    tests_res = pca.transform(tests)
+    print(tests_res)
+    print(pca.decode(tests_res))
+
+
+test()
