293caf9ee4a9385eb68ab28106fa5c42da6f0b54
qduval <qduval@murex.com>
2019-10-04 12:34:27 +0200

    Linear regression

--- ml/linear_regression.py
+++ ml/linear_regression.py
@@ -72,6 +72,11 @@ Solving techniques
 
 import numpy as np
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+
 
 def linear_regression(xs, ys):
     X = np.stack(np.array(x, dtype=np.float32) for x in xs)
@@ -86,6 +91,35 @@ def linear_regression(xs, ys):
         return None
 
 
+def linear_regression_dl(xs, ys):
+    criterion = nn.MSELoss()
+    model = nn.Linear(len(xs[0]), 1)
+    model.train()
+
+    optimizer = optim.Adam(model.parameters(), lr=1e-2, weight_decay=0.)
+    inputs = torch.stack([torch.FloatTensor(x) for x in xs])
+    training_set = TensorDataset(inputs, torch.FloatTensor(ys))
+    training_loader = DataLoader(training_set, batch_size=100, shuffle=True)
+
+    # TODO - continue while the loss decreases (stop when it does not improve)
+    # TODO - check how we can get rid of the reshape
+    # TODO - implement the derivative stuff by hand
+
+    for _ in range(10_000):
+        total_loss = 0.
+        for xs, ys in training_loader:
+            optimizer.zero_grad()
+            output = model(xs)
+            loss = criterion(output.reshape((5,)), ys)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        print(total_loss)
+
+    model.eval()
+    return model.weight, model.bias     # TODO - return the weights, not tensors
+
+
 # Non singular matrix are solved correctly
 ws = linear_regression(xs=[(3,), (5,), (7,), (9,), (11,)], ys=[4, 4, 6, 8, 8])
 print(ws)
@@ -94,4 +128,6 @@ print(ws)
 ws = linear_regression(xs=[(3,1), (5,1), (7,1), (9,1), (11,1)], ys=[4, 4, 6, 8, 8])
 print(ws)
 
-
+# Now with deep learning
+ws = linear_regression_dl(xs=[(3,), (5,), (7,), (9,), (11,)], ys=[4, 4, 6, 8, 8])
+print(ws)
