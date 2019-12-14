43c7f65db0b3f51e1b38e20ac8605c3e56befd4f
duvalquentin <senmenty@gmail.com>
2019-12-14 18:56:23 +0100

    Space filling

--- ml/codingame/tron.py
+++ ml/codingame/tron.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from collections import deque
 from typing import List, NamedTuple, Tuple
 
 
@@ -39,6 +40,9 @@ ALL_MOVES = [
     Move.create("DOWN", 0, 1)]
 
 
+ALL_DIRECTIONS = [(move.direction[0], move.direction[1]) for move in ALL_MOVES]
+
+
 """
 World grid
 """
@@ -50,8 +54,16 @@ EMPTY = -1
 
 
 class World:
-    def __init__(self):
-        self.grid = np.full(shape=(WIDTH, HEIGHT), fill_value=EMPTY, dtype=np.int)
+    def __init__(self, grid):
+        self.grid = grid
+
+    @classmethod
+    def empty(cls):
+        grid = np.full(shape=(WIDTH, HEIGHT), fill_value=EMPTY, dtype=np.int)
+        return World(grid)
+
+    def clone(self):
+        return World(np.copy(self.grid))
 
     def remove_player(self, player_id: int):
         debug("REMOVE PLAYER:", player_id)
@@ -70,9 +82,24 @@ class World:
         x, y = position
         self.grid[(x, y)] = player_id
 
-    def play(self, player_id: int, position: Vector, move: Move):
-        x, y = move.apply(position)
-        self.grid[(x, y)] = player_id
+    def flood_fill(self, positions: List[Vector]) -> List[int]:
+        scores = [0] * len(positions)
+        filled = np.copy(self.grid)
+        to_visit = deque([(x, y) for x, y in positions if filled[(x, y)] != EMPTY])
+        while to_visit:
+            x, y = to_visit.popleft()
+            owner = filled[(x, y)]
+            if owner == -1:
+                continue
+
+            for dx, dy in ALL_DIRECTIONS:
+                next_pos = x + dx, y + dy
+                if 0 <= next_pos[0] < WIDTH and 0 <= next_pos[1] < HEIGHT:
+                    if filled[next_pos] == EMPTY:
+                        filled[next_pos] = owner
+                        scores[owner] += 1
+                        to_visit.append(next_pos)
+        return scores
 
 
 """
@@ -85,13 +112,26 @@ class Agent:
         self.world = world
         self.prediction = None
 
-    def get_action(self, positions: List[Vector], player_index: int) -> Move:
-        player_pos = positions[player_index]
+    def get_action(self, positions: List[Vector], player_id: int) -> Move:
+        debug(self.world.flood_fill(positions))
+        player_pos = positions[player_id]
         self._check_prediction(player_pos)
-        valid_moves = world.valid_moves(player_pos)
+        valid_moves = self.world.valid_moves(player_pos)
+
+        best_move = None
+        max_score = 0
         for move in valid_moves:
-            self.prediction = move.apply(player_pos)
-            return move
+            world = self.world.clone()
+            next_positions = list(positions)
+            next_positions[player_id] = move.apply(player_pos)
+            world.acquire(player_id, next_positions[player_id])
+            scores = world.flood_fill(next_positions)
+            debug(move, scores)
+            if scores[player_id] > max_score:
+                max_score = scores[player_id]
+                best_move = move
+        self.prediction = best_move.apply(player_pos)
+        return best_move
 
     def _check_prediction(self, player_pos: Vector):
         if self.prediction is not None and not np.array_equal(self.prediction, player_pos):
@@ -130,8 +170,14 @@ Game loop
 """
 
 
-world = World()
-agent = Agent(world)
+# TODO - high level AI: beginning of game, loosing game, end of game, etc.
+# TODO - minimax
+# TODO - beware of suicidal tendencies of minimax
+# TODO - better evaluation that choosing your best score: you need to penalize the opponent as well (pick the higher)
+
+
+global_world = World.empty()
+agent = Agent(global_world)
 
 prev_inputs = None
 prediction = None
@@ -142,9 +188,10 @@ while True:
         for player_id in range(inputs.nb_player):
             if inputs.is_over(player_id):
                 if not prev_inputs.is_over(player_id):
-                    world.remove_player(player_id)
+                    global_world.remove_player(player_id)
             else:
-                world.acquire(player_id, inputs.curr_pos[player_id])
+                global_world.acquire(player_id, inputs.init_pos[player_id])
+                global_world.acquire(player_id, inputs.curr_pos[player_id])
 
     debug(inputs)
     action = agent.get_action(inputs.curr_pos, inputs.player_index)
