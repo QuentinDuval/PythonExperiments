44d6a17e2acf136274f73a4d3acc1f48b1a16afd
duvalquentin <senmenty@gmail.com>
2020-01-10 16:55:01 +0100

    Code for the collisions (first try)

--- ml/codingame/fantastic_bits_ga.py
+++ ml/codingame/fantastic_bits_ga.py
@@ -104,6 +104,9 @@ class Entities:
     radius: np.ndarray
     masses: np.ndarray
 
+    def __len__(self):
+        return self.positions.shape[0]
+
 
 def normal_of(p1: Vector, p2: Vector) -> Vector:
     n = np.array([p1[1] - p2[1], p2[0] - p1[0]])
@@ -139,5 +142,60 @@ def find_collision(entities: Entities, i1: int, i2: int, dt: float) -> float:
     return distance_to_intersection / norm(speed)
 
 
+def find_first_collision(entities: Entities, dt: float = 1.0) -> Tuple[int, int, float]:
+    # TODO - take into account the walls ! (return id=-1)
+    low_t = float('inf')
+    best_i = 0
+    best_j = 0
+    n = len(entities)
+    for i in range(n):
+        for j in range(i, n):
+            t = find_collision(entities, i, j, dt)
+            if t < low_t:
+                best_i = i
+                best_j = j
+    return best_i, best_j, low_t
+
+
+def move_time_forward(entities: Entities, dt: float = 1.0):
+    entities.positions += entities.speeds * dt
+
+
+def bounce(entities: Entities, i1: int, i2: int, min_impulsion: float):
+
+    # Getting the masses
+    m1 = entities.masses[i1]
+    m2 = entities.masses[i2]
+    mcoeff = (m1 + m2) / (m1 * m2)
+
+    # Difference of position and speeds
+    dp12 = entities.positions[i2] - entities.positions[i1]
+    dv12 = entities.speeds[i2] - entities.speeds[i1]
+
+    # Computing the force
+    product = np.dot(dp12, dv12)
+    d12_squared = np.dot(dp12, dp12)
+    f12 = dp12 * product / (d12_squared * mcoeff)
+
+    # Apply the force (first time)
+    entities.speeds[i1] -= f12 / m1
+    entities.speeds[i2] += f12 / m2
+
+    # Minimum impulsion
+    norm_f = norm(f12)
+    if norm_f < min_impulsion:
+        f12 *= min_impulsion / norm_f
+
+    # Apply the force (second time)
+    entities.speeds[i1] -= f12 / m1
+    entities.speeds[i2] += f12 / m2
+
+
 def simulate_collisions(entities: Entities, dt: float = 1.0) -> Entities:
-    pass
+    while dt > 0.:
+        i, j, t = find_first_collision(entities, dt)    # TODO - beware when t is 0
+        move_time_forward(entities, t)
+        bounce(entities, i, j, min_impulsion=100.)      # TODO - min_impulsion is 0. against walls
+        dt -= t
+    return entities
+
