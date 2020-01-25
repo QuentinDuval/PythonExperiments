be29aa83a4ee51604d33cc88a07383a702589a89
duvalquentin <senmenty@gmail.com>
2020-01-25 11:10:18 +0100

    Finally a working AI again (after refactoring)

--- ml/codingame/code_busters.py
+++ ml/codingame/code_busters.py
@@ -1,4 +1,6 @@
+import abc
 import copy
+import enum
 from dataclasses import *
 import math
 import sys
@@ -81,42 +83,6 @@ def mod_angle(angle: Angle) -> Angle:
 
 """
 ------------------------------------------------------------------------------------------------------------------------
-ACTION
-------------------------------------------------------------------------------------------------------------------------
-"""
-
-
-class Move(NamedTuple):
-    position: np.ndarray
-
-    def __repr__(self):
-        x, y = self.position
-        return "MOVE " + str(int(x)) + " " + str(int(y))
-
-
-class Bust(NamedTuple):
-    ghost_id: int
-
-    def __repr__(self):
-        return "BUST " + str(self.ghost_id)
-
-
-class Release:
-    def __repr__(self):
-        return "RELEASE"
-
-
-class Stun(NamedTuple):
-    buster_id: int
-
-    def __repr__(self):
-        return "STUN " + str(self.buster_id)
-
-
-Action = Union[Move, Bust, Release, Stun]
-
-"""
-------------------------------------------------------------------------------------------------------------------------
 MAIN DATA STRUCTURE to keep STATE
 ------------------------------------------------------------------------------------------------------------------------
 """
@@ -134,14 +100,18 @@ MIN_BUST_DISTANCE = 900
 MAX_BUST_DISTANCE = 1760
 MAX_STUN_DISTANCE = 1760
 
+STUN_DURATION = 10
 STUN_COOLDOWN = 20
 
 TEAM_CORNERS = np.array([[0, 0], [WIDTH, HEIGHT]], dtype=np.float32)
 
 
+EntityId = int;
+
+
 @dataclass(frozen=False)
 class Ghost:
-    uid: int
+    uid: EntityId
     position: np.ndarray
     endurance: int
     busters_count: int = 0
@@ -153,7 +123,7 @@ class Ghost:
 
 @dataclass(frozen=False)
 class Buster:
-    uid: int
+    uid: EntityId
     position: np.ndarray
     team: int  # 0 for team 0, 1 for team 1
     carried_ghost: int  # ID of the ghost being carried, -1 if no ghost carried
@@ -169,6 +139,7 @@ class Buster:
 @dataclass(frozen=False)
 class Entities:
     my_team: int
+    my_score: int
     busters_per_player: int
     ghost_count: int
     busters: Dict[int, Buster] = field(default_factory=dict)
@@ -196,7 +167,8 @@ READING & TRACKING GAME STATE
 
 
 def read_entities(my_team_id: int, busters_per_player: int, ghost_count: int, previous_entities: Entities):
-    entities = Entities(my_team=my_team_id, busters_per_player=busters_per_player, ghost_count=ghost_count)
+    entities = Entities(my_team=my_team_id, my_score=0, busters_per_player=busters_per_player, ghost_count=ghost_count)
+    carried_ghosts = set()
     n = int(input())
     for i in range(n):
         # entity_id: buster id or ghost id
@@ -224,19 +196,96 @@ def read_entities(my_team_id: int, busters_per_player: int, ghost_count: int, pr
                 carried_ghost=value if state == 1 else -1,
                 stun_duration=value if state == 2 else 0,
                 busting_ghost=state == 3)
+            carried_ghosts.add(value if state == 1 else -1)
+
+    # IMPORTANT NODE:
+    # * the carried ghosts are not part of the input
+
     if not previous_entities:
         return entities
 
-    # TODO - beware - ghosts might disappear - make them move for obvious ones
-    for ghost_id, ghost in previous_entities.ghosts.items():
-        if ghost_id not in entities.ghosts and ghost.last_seen <= 10:  # TODO - parameterize
-            entities.ghosts[ghost_id] = ghost
+    # Adding the previous information
+    entities.my_score = previous_entities.my_score
+    for buster in entities.get_player_busters():
+        buster.stun_cooldown = previous_entities.busters[buster.uid].stun_cooldown
+        # TODO - probably missing stuff here
 
-    # TODO - beware these players can move - make them move for obvious ones
+    # Adding missing ghosts
+    for ghost_id, ghost in previous_entities.ghosts.items():
+        if ghost_id not in entities.ghosts and ghost_id not in carried_ghosts and ghost.last_seen <= 10:  # TODO - parameterize
+            entities.ghosts[ghost_id] = ghost
+            ghost.last_seen += 1
+
+    # Adding missing busters
     for buster in previous_entities.get_opponent_busters():
         if buster.uid not in entities.busters and buster.last_seen <= 10:  # TODO - parameterize
             entities.busters[buster.uid] = buster
+            buster.last_seen += 1
     return entities
+
+
+def on_end_of_turn(entities: Entities):
+    # TODO - make ghosts move - or make it as ACTION
+    # TODO - make opponent busters carrying ghosts move - or make it as ACTION
+    # TODO - decrease all the cooldowns
+    pass
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+ACTION
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+class Move(NamedTuple):
+    caster_id: int
+    position: np.ndarray
+
+    def apply(self, entities: Entities):
+        pass  # TODO - no need now
+
+    def __repr__(self):
+        x, y = self.position
+        return "MOVE " + str(int(x)) + " " + str(int(y))
+
+
+class Bust(NamedTuple):
+    caster_id: int
+    target_id: int
+
+    def apply(self, entities: Entities):
+        pass  # TODO - no need now
+
+    def __repr__(self):
+        return "BUST " + str(self.target_id)
+
+
+class Release(NamedTuple):
+    caster_id: int
+    target_id: int
+
+    def apply(self, entities: Entities):
+        entities.busters[self.caster_id].carried_ghost = -1
+        del entities.ghosts[self.target_id]
+
+    def __repr__(self):
+        return "RELEASE"
+
+
+class Stun(NamedTuple):
+    caster_id: int
+    target_id: int
+
+    def apply(self, entities: Entities):
+        entities.busters[self.caster_id].stun_cooldown = STUN_COOLDOWN
+        entities.busters[self.target_id].stun_duration = STUN_DURATION
+
+    def __repr__(self):
+        return "STUN " + str(self.target_buster_id)
+
+
+Action = Union[Move, Bust, Release, Stun]
 
 
 """
@@ -269,9 +318,9 @@ class Territory:
     def __len__(self):
         return len(self.unvisited)
 
-    def assign_destinations(self, entities: Entities) -> Dict[int, np.ndarray]:
+    def assign_destinations(self, busters: Collection[Buster]) -> Dict[int, Vector]:
         heap = []
-        for buster in entities.get_player_busters():
+        for buster in busters:
             for point in self.unvisited:
                 d = distance2(point, buster.position)
                 x, y = point
@@ -280,7 +329,7 @@ class Territory:
 
         assignments = {}
         taken = set()
-        while heap and len(assignments) < entities.busters_per_player:
+        while heap and len(assignments) < len(busters):
             d, player_id, point = heapq.heappop(heap)
             if player_id not in assignments:
                 if point not in taken:
@@ -288,12 +337,15 @@ class Territory:
                     taken.add(point)
         return assignments
 
-    def track_explored(self, entities: Entities):
+    def track_explored(self, busters: Iterator[Buster]):
         # TODO - inefficient (look only neighbors)
-        for buster in entities.get_player_busters():
+        for buster in busters:
             for point in list(self.unvisited):
                 if distance2(point, buster.position) < self.cell_dist2:
                     self.unvisited.discard(point)
+
+    def is_visited(self, point: Vector) -> bool:
+        return point not in self.unvisited
 
 
 """
@@ -319,98 +371,139 @@ AGENT
 # TODO - consider going for a stun if the opponent carries a ghost - pursing him, etc
 
 
+@dataclass(frozen=False)
+class Exploring:
+    destination: Vector
+
+
+@dataclass(frozen=False)
+class Capturing:
+    target_id: EntityId
+
+
+@dataclass(frozen=False)
+class Carrying:
+    pass
+
+
+@dataclass(frozen=False)
+class Escorting:
+    pass
+
+
+@dataclass(frozen=False)
+class Intercepting:
+    pass
+
+
+@dataclass(frozen=False)
+class Herding:
+    pass
+
+
+# TODO - add a state for the game as well (exploration phase, mid-game, end-game)...
+
+
 class Agent:
     def __init__(self):
-        self.territory = Territory()
-        self.actions: Dict[int, Action] = {}
         self.chrono = Chronometer()
+        self.territory = Territory()
+        self.unassigned: Set[int] = set()
+        self.exploring: Dict[int, Exploring] = {}
+        self.capturing: Dict[int, Capturing] = {}
+        self.carrying: Dict[int, Carrying] = {}
+
+    def init(self, entities: Entities):
+        busters = list(entities.get_player_busters())
+        assignments = self.territory.assign_destinations(busters)
+        for buster in entities.get_player_busters():
+            self.exploring[buster.uid] = Exploring(assignments[buster.uid])
 
     def get_actions(self, entities: Entities) -> List[Action]:
         self.chrono.start()
-        self.actions.clear()
-        self.territory.track_explored(entities)
 
-        # TODO - first do an analysis of situation to transition phases?
-
-        self.if_has_ghost_go_to_base(entities)
-        self.stun_closest_opponents(entities)
-        self.go_fetch_closest_ghosts(entities)
-        self.go_explore_territory(entities)
-        self.go_to_middle(entities)
+        self._update_past_states(entities)
+        self._state_transitions(entities)
+        actions = self._carry_actions(entities)
 
         debug("Time spent:", self.chrono.spent())
-        return [action for _, action in sorted(self.actions.items(), key=lambda p: p[0])]
+        return [p[1] for p in sorted(actions.items(), key=lambda p: p[0])]
 
-    def if_has_ghost_go_to_base(self, entities: Entities):
-        for buster in entities.get_player_busters():
-            if buster.carried_ghost >= 0:
-                player_corner = TEAM_CORNERS[entities.my_team]
-                if distance2(buster.position, player_corner) < RADIUS_BASE ** 2:
-                    self.actions[buster.uid] = Release()
-                    del entities.ghosts[buster.carried_ghost]
-                else:
-                    self.actions[buster.uid] = Move(player_corner)
+    def _update_past_states(self, entities: Entities):
 
-    def stun_closest_opponents(self, entities: Entities):
-        busters = entities.get_player_busters()
-        opponents = entities.get_opponent_busters()
-        for opponent in opponents:
-            if opponent.stun_duration <= 1:  # 1 allows to stun-lock but avoids double stuns
-                for buster in busters:
-                    if buster.uid not in self.actions and buster.stun_cooldown <= 0:
-                        if distance2(buster.position, opponent.position) < MAX_STUN_DISTANCE ** 2:
-                            self.actions[buster.uid] = Stun(opponent.uid)
-                            entities.stun_cooldown = STUN_COOLDOWN
-                        elif distance2(buster.position, opponent.position) < RADIUS_SIGHT ** 2:
-                            self.actions[buster.uid] = Move(opponent.position)
+        # Tracking exploration state
+        self.territory.track_explored(entities.get_player_busters())
+        for buster_id, exploring in list(self.exploring.items()):
+            if self.territory.is_visited(exploring.destination):
+                del self.exploring[buster_id]
+                self.unassigned.add(buster_id)
 
-    def go_fetch_closest_ghosts(self, entities: Entities):
-        if not entities.ghosts:
-            return
+        # Carrying update
+        for buster_id, carrying in list(self.carrying.items()):
+            if entities.busters[buster_id].carried_ghost < 0:
+                del self.carrying[buster_id]
+                self.unassigned.add(buster_id)
 
-        # Prioritize the ghosts with low endurance - TODO - take into account distance + distance to base as well => mix
+        # Capturing update
+        for buster_id, capturing in list(self.capturing.items()):
+            if entities.busters[buster_id].carried_ghost >= 0:
+                del self.capturing[buster_id]
+                self.carrying[buster_id] = Carrying()
+            elif not entities.busters[buster_id].busting_ghost:
+                del self.capturing[buster_id]
+                self.unassigned.add(buster_id)
+
+    def _carry_actions(self, entities: Entities) -> Dict[int, Action]:
+        actions = dict()
+
+        # Exploration of the map
+        for buster_id, exploring in self.exploring.items():
+            actions[buster_id] = Move(buster_id, exploring.destination)
+
+        # Capturing a ghost
+        for buster_id, capturing in self.capturing.items():
+            buster = entities.busters[buster_id]
+            ghost = entities.ghosts[capturing.target_id]
+            if distance2(buster.position, ghost.position) > MAX_BUST_DISTANCE ** 2:
+                actions[buster_id] = Move(buster_id, ghost.position)   # TODO - anticipate move of ghost?
+            else:
+                actions[buster_id] = Bust(buster_id, capturing.target_id)
+
+        # Carrying ghost to base
+        for buster_id, carrying in self.carrying.items():
+            buster = entities.busters[buster_id]
+            team_corner = TEAM_CORNERS[entities.my_team]
+            if distance2(team_corner, buster.position) < RADIUS_BASE ** 2:
+                actions[buster_id] = Release(buster_id, buster.carried_ghost)
+            else:
+                actions[buster_id] = Move(buster_id, team_corner)
+        return actions
+
+    def _state_transitions(self, entities: Entities):
         ghosts: List[Ghost] = list(entities.ghosts.values())
-        ghosts.sort(key=lambda g: g.endurance)
-        busters: List[Buster] = list(entities.get_player_busters())
 
-        debug(ghosts)
-        debug(busters)
+        debug(self.unassigned)
+        debug(self.exploring)
+        debug(self.capturing)
+        debug(self.carrying)
 
-        for ghost in ghosts:
-            busters.sort(key=lambda b: distance2(ghost.position, b.position))
+        # Assign vacant busters
+        # TODO - avoid assigning too many to the same buster
+        vacant_busters = [entities.busters[uid] for uid in self.unassigned]
+        assignments = self.territory.assign_destinations(vacant_busters)
+        for buster_id in self.unassigned:
+            tile_pos = assignments[buster_id]
+            buster = entities.busters[buster_id]
+            best_ghost = min(ghosts, key=lambda g: self._ghost_score(buster, g), default=None)
+            if best_ghost and self._ghost_score(buster, best_ghost) < distance2(tile_pos, buster.position):
+                self.capturing[buster_id] = Capturing(target_id=best_ghost.uid)
+            else:
+                self.exploring[buster_id] = Exploring(destination=tile_pos)
 
-            # TODO - take into account move of ghosts to choose where to go
-            # TODO - compute a number of busters to assign? Should instead have a kind of priority
-            for buster in busters:
-                if buster.uid not in self.actions:
-                    closest_dist2 = distance2(ghost.position, buster.position)
-                    if closest_dist2 == 0:
-                        next_player_pos = np.array([WIDTH / 2, HEIGHT / 2])
-                        self.actions[buster.uid] = Move(next_player_pos)
-                    elif closest_dist2 < MIN_BUST_DISTANCE ** 2:
-                        next_player_dir = buster.position - ghost.position
-                        next_player_pos = buster.position + next_player_dir / norm(next_player_dir) * (
-                                    MAX_MOVE_DISTANCE - GHOST_MOVE_DISTANCE)
-                        self.actions[buster.uid] = Move(next_player_pos)
-                    elif closest_dist2 < MAX_BUST_DISTANCE ** 2:
-                        self.actions[buster.uid] = Bust(ghost.uid)
-                    else:
-                        self.actions[buster.uid] = Move(ghost.position)
+        self.unassigned.clear()
 
-                    # Do not put too many busters on a weak ghost
-                    if ghost.endurance <= 5:
-                        break
-
-    def go_explore_territory(self, entities: Entities):
-        assignments = self.territory.assign_destinations(entities)
-        for player_id, point in assignments.items():
-            if player_id not in self.actions:
-                self.actions[player_id] = Move(point)
-
-    def go_to_middle(self, entities: Entities):
-        for buster in entities.get_player_busters():
-            if buster.uid not in self.actions:
-                self.actions[buster.uid] = Move(np.ndarray([8000, 4500]))
+    def _ghost_score(self, buster: Buster, ghost: Ghost):
+        return distance2(ghost.position, buster.position) / 800 ** 2 + ghost.endurance
 
 
 """
@@ -431,17 +524,20 @@ def game_loop():
 
     agent = Agent()
     previous_entities = None
-    while True:
+    for turn_nb in itertools.count(start=0, step=1):
         entities = read_entities(my_team_id=my_team_id,
                                  busters_per_player=busters_per_player,
                                  ghost_count=ghost_count,
                                  previous_entities=previous_entities)
         debug(entities)
 
-        actions = agent.get_actions(entities)
-        for action in actions:
+        if turn_nb == 0:
+            agent.init(entities)
+
+        for action in agent.get_actions(entities):
             print(action)
 
+        on_end_of_turn(entities)
         previous_entities = entities
 
 
