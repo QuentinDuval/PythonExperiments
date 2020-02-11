debd9cc2f3dc7edf23630dba0ffb2b00cbe2e655
duvalquentin <senmenty@gmail.com>
2020-02-11 11:03:24 +0100

    Ideas

--- ml/codingame/Zombies.py
+++ ml/codingame/Zombies.py
@@ -177,7 +177,7 @@ class Agent:
 
         # Go for the zombie directly if it is closer than the human, else: intercept
         if distance2(game_state.player, closest_zombie.position) < distance2(game_state.player, closest_human.position):
-            # TODO - You could actually attract the zombie...
+            # TODO - You could actually attract the zombie... if all the zombies are away, go to the point at equal distance to all of them
             target_position = closest_zombie.position
         else:
             target_position = (closest_human.position + closest_zombie.position) / 2
