6569110bfe9971eee369594a7015f2611a1737e4
duvalquentin <senmenty@gmail.com>
2020-02-13 10:45:27 +0100

    The basis (skeleton code)

---
+++ ml/codingame/ghost_in_the_cell.py
@@ -0,0 +1,137 @@
+from collections import *
+import itertools
+import math
+import sys
+import time
+from dataclasses import *
+from typing import *
+
+import numpy as np
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+UTILS
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+def debug(*args):
+    print(*args, file=sys.stderr)
+
+
+class Chronometer:
+    def __init__(self):
+        self.start_time = 0
+
+    def start(self):
+        self.start_time = time.time_ns()
+
+    def spent(self):
+        current = time.time_ns()
+        return self._to_ms(current - self.start_time)
+
+    @staticmethod
+    def _to_ms(delay):
+        return delay / 1_000_000
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+GAME ENTITIES
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+Distance = int
+EntityId = int
+Topology = Dict[EntityId, Dict[EntityId, Distance]]
+
+
+@dataclass()
+class Factory:
+    owner: int
+    cyborg_count: int
+    production: int
+
+
+@dataclass()
+class Troop:
+    owner: int
+    source: EntityId
+    destination: EntityId
+    cyborg_count: int
+    distance: Distance
+
+
+@dataclass()
+class GameState:
+    factories: Dict[EntityId, Factory] = field(default_factory=dict)
+    troops: Dict[EntityId, Troop] = field(default_factory=dict)
+
+    @classmethod
+    def read(cls):
+        state = cls()
+        entity_count = int(input())
+        for i in range(entity_count):
+            entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 = input().split()
+            entity_id = int(entity_id)
+            arg_1 = int(arg_1)
+            arg_2 = int(arg_2)
+            arg_3 = int(arg_3)
+            arg_4 = int(arg_4)
+            arg_5 = int(arg_5)
+            if entity_type == "FACTORY":
+                state.factories[entity_id] = Factory(owner=arg_1, cyborg_count=arg_2, production=arg_3)
+            elif entity_type == "TROOP":
+                state.troops[entity_id] = Troop(
+                    owner=arg_1, source=arg_2, destination=arg_3,
+                    cyborg_count=arg_4, distance=arg_5)
+        return state
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+AGENT
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+class Agent:
+    def __init__(self):
+        self.chrono = Chronometer()
+
+    def get_action(self, topology: Topology, game_state: GameState) -> str:
+        # Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
+        return "WAIT"
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+GAME LOOP
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+def read_topology() -> Topology:
+    topology = defaultdict(lambda: defaultdict(Distance))
+    factory_count = int(input())
+    link_count = int(input())
+    for i in range(link_count):
+        factory_1, factory_2, distance = [int(j) for j in input().split()]
+        topology[factory_1][factory_2] = distance
+        topology[factory_2][factory_1] = distance
+    return topology
+
+
+def game_loop():
+    agent = Agent()
+    topology = read_topology()
+    while True:
+        game_state = GameState.read()
+        action = agent.get_action(topology, game_state)
+        print(action)
+
+
+if __name__ == '__main__':
+    game_loop()
