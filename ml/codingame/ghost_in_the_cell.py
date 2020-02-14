b29890e121ff4d0a77031da962b53427a761ce06
duvalquentin <senmenty@gmail.com>
2020-02-14 11:55:23 +0100

    Ideas

--- ml/codingame/ghost_in_the_cell.py
+++ ml/codingame/ghost_in_the_cell.py
@@ -258,9 +258,7 @@ class Agent:
         actions: Actions = []
 
         # TODO - increase action
-        # TODO - pre-treat the topology to detect your camp and defend it
-        # TODO - pre-treat the topology to pass through as many  nodes as possible
-        # TODO - apply the effect of on-going actions
+        # TODO - apply the effect of on-going actions to defend efficiently
 
         for f_id, f in game_state.factories.items():
             if f.owner > 0 and f.cyborg_count > self.MIN_TROOPS:
