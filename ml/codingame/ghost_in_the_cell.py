419002e800dec7cae924ecedf45708a227dc3f46
duvalquentin <senmenty@gmail.com>
2020-02-15 11:10:38 +0100

    Insight - attacking is great

--- ml/codingame/ghost_in_the_cell.py
+++ ml/codingame/ghost_in_the_cell.py
@@ -317,19 +317,21 @@ class Agent:
                         game_state: GameState, excess_cyborgs: int,
                         bomb_impacts: Dict[EntityId, Distance]) -> Actions:
 
-        # TODO - shooting opponent cyborgs is better than shooting neutral cyborgs!!!
-
         def attractiveness(f_id: int):
+            f = game_state.factories[f_id]
+
             # TODO - take into account dist with hops
             dist = topology.distance(source, f_id)
             if dist < bomb_impacts.get(f_id, -INF_DISTANCE):
                 return float('inf')
 
+            # Shooting opponent cyborgs is better than shooting neutral cyborgs!!!
+            opponent_factor = 1 if f.owner == -1 else 2 # TODO - improve
+
             # TODO - in the beginning of the game, tend to waste cyborgs on targets highly occupied
             # TODO => tend to push toward different AI depending on whether I win or lose
-            f = game_state.factories[f_id]
             camp_factor = 1 if topology.get_camp(f_id) == 1 else 5  # More attractiveness for my camp
-            return camp_factor * (f.projected_count ** 2 + 1) * dist / (f.production ** 2 + 1)
+            return camp_factor * (f.projected_count ** opponent_factor + 1) * dist / (f.production ** 2 + 1)
 
         targets = []
         for f_id, f in game_state.factories.items():
