d402d7847e0e788eef886641f830976b59c63755
duvalquentin <senmenty@gmail.com>
2020-03-09 11:09:59 +0100

    Exercises

--- classics/MinStack.py
+++ classics/MinStack.py
@@ -1,10 +1,34 @@
 """
-https://practice.geeksforgeeks.org/problems/special-stack/1
-
-Implement a stack with the following operations in O(1):
-- push
-- pop
-- min
+https://leetcode.com/problems/min-stack/
 """
 
-# TODO
+
+class MinStack:
+    """
+    Store the mins for every values... but then look at what it looks like
+    => get rid of everytime you store the min different than the current val
+    """
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def push(self, x: int) -> None:
+        if not self.stack:
+            self.stack.append(x)
+            self.mins.append(x)
+        else:
+            self.stack.append(x)
+            if x <= self.mins[-1]:
+                self.mins.append(x)
+
+    def pop(self) -> None:
+        x = self.stack.pop()
+        if x == self.mins[-1]:
+            self.mins.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
