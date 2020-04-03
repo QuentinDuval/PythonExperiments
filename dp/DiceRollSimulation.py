710b7d5223ac5c71a5c42d3ef0a91d09e997ed03
duvalquentin <senmenty@gmail.com>
2020-04-03 10:29:54 +0200

    Read the problem correctly...

--- dp/DiceRollSimulation.py
+++ dp/DiceRollSimulation.py
@@ -1,3 +1,16 @@
+"""
+https://leetcode.com/problems/dice-roll-simulation/
+
+A die simulator generates a random number from 1 to 6 for each roll. You introduced a constraint to the generator such
+that it cannot roll the number i more than rollMax[i] (1-indexed) consecutive times.
+
+Given an array of integers rollMax and an integer n, return the number of distinct sequences that can be obtained
+with exact n rolls.
+
+Two sequences are considered different if at least one element differs from each other. Since the answer may be
+too large, return it modulo 10^9 + 7
+"""
+
 from functools import lru_cache
 from typing import List
 
